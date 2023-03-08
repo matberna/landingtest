f340b0df33e6528f662321c212293a9e5264f11d
matberna <matteo.bernardini@cepu.it>
2023-03-08 16:02:25 +0100

    upgrd

--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
 import i18next from "i18next";
 import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
-// We make the function async so we can await
-// the translation file as it pipes down the
-// network
+
 async function initI18next() {
-    await i18next
-      .use(HttpApi)
-      .use(LanguageDetector)
-      .init({
-        debug: true,
-        supportedLngs: ["en", "it", "fr", "es", "de"],
-        fallbackLng: "en",
-        // Allow "en" to be used for
-        // "en-US", "en-CA", etc.
-        nonExplicitSupportedLngs: true,
-        backend: {
-          loadPath: "/lang/{{lng}}.json",
-        },
-      });
-  }
-// Quick refactor of the page translation code
-// to a function
+  await i18next
+    .use(HttpApi)
+    .use(LanguageDetector)
+    .init({
+      debug: true,
+      supportedLngs: ["en", "it", "fr", "es", "de", "ar"],
+      fallbackLng: "en",
+      nonExplicitSupportedLngs: true,
+      backend: {
+        loadPath: "/lang/{{lng}}.json",
+      },
+    });
+}
+
 function translatePageElements() {
-  const translatableElements = document.querySelectorAll(
-    "[data-i18n-key]",
-  );
+  setHTML_lang(i18next.language);
+  const translatableElements = document.querySelectorAll("[data-i18n-key]");
   translatableElements.forEach((el) => {
     const key = el.getAttribute("data-i18n-key");
     el.innerHTML = i18next.t(key);
@@ -33,16 +27,25 @@ function translatePageElements() {
 }
 
 function bindLocaleSwitcher(initialValue) {
-    const switcher = document.querySelector(
-      "[data-i18n-switcher]",
-    );
-    switcher.value = initialValue;
-    switcher.onchange = (e) => {
-      i18next
-        .changeLanguage(e.target.value)
-        .then(translatePageElements);
-    };
-  }
+  const switcher = document.querySelector(
+    "[data-i18n-switcher]",
+  );
+  switcher.value = initialValue;
+  switcher.onchange = (e) => {
+    i18next
+      .changeLanguage(e.target.value)
+      .then(translatePageElements);
+  };
+}
+
+function setHTML_lang(locale) {
+  document.documentElement.dir = dir(i18next.resolvedLanguage);
+  document.documentElement.lang = i18next.resolvedLanguage;
+}
+
+function dir(locale) {
+  return locale === "ar" ? "rtl" : "ltr";
+}
 
 // Init
 (async function () {
